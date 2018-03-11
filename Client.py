66e9801ae94089f278b142b7175fa8d7d46a1813
Harsh <32223129+HarshV8@users.noreply.github.com>
2018-03-11 14:42:13 -0400

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,37 @@
+#CLIENT CODE
+
+import socket
+import threading
+
+
+HOST = '0.0.0.0'
+PORT = 5559
+print("[+] Client Running ")
+HOST = str(input("[+] Enter Destination IP : "))
+PORT = int(input("[+] Enter Port : "))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+class myThread(threading.Thread):
+    def __init__(self,id):
+        threading.Thread.__init__(self)
+        self.threadID = id
+
+    def run(self):
+        print("[+] Listing On Thread "+str(self.threadID))
+        while 1:
+            data = s.recv(1024)
+            data_recieved = data.decode()
+            if(data_recieved!=""):
+                print("Recieved : "+str(data_recieved))
+
+Listening_Thread = myThread(1)
+Listening_Thread.start()
+
+while 1:
+    sending_data = input("")
+    sending_bytes = sending_data.encode()
+    s.send(sending_bytes)
+    #print("Sent : "+str(sending_data))
+s.close()
---
+++ Server.py
@@ -0,0 +1,43 @@
+#SERVER CODE
+
+import sys
+import socket
+import threading
+
+HOST = '0.0.0.0'
+if(len(sys.argv)==1):
+    PORT = 5559
+elif(len(sys.argv)==2):
+    PORT=int(sys.argv[1])
+
+print("[+] Server Running ")
+print("[+] Allowing All Incoming Connections ")
+print("[+] PORT "+str(PORT))
+print("[+] Waiting For Connection...")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print('[+] Connected by ', addr)
+
+class myThread(threading.Thread):
+    def __init__(self,id):
+        threading.Thread.__init__(self)
+        self.threadID = id
+
+    def run(self):
+        print("[+] Listing On Thread "+str(self.threadID))
+        while 1:
+            data = conn.recv(1024)
+            data_recieved = data.decode()
+            if(data_recieved != ""):
+                print("Recieved : "+str(data_recieved))
+
+Listening_Thread = myThread(1)
+Listening_Thread.start()
+
+while 1:
+    sending_data = str(input(""))
+    conn.send(sending_data.encode())
+    #print("Sending : "+str(sending_data))
+conn.close()
