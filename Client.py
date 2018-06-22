385e4dd98dd75b8269379062036fd5a0fe13f85a
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-06-23 01:39:24 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -15,14 +15,19 @@ except ImportError:
 import socket
 import threading
 import hashlib
+import json
+from datetime import datetime
 
 print("[+] Client Running ")
-HOST = str(input("[+] Enter Destination IP : "))
-PORT = int(input("[+] Enter Port : "))
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-key = str(input("[+] Enter AES Encryption Key For Connection : "))
+HOST = str(input('[+] Enter Destination IP   : '))
+PORT = int(input('[+] Enter Destination Port : '))
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+except ConnectionError:
+    print('Could Not Connect !')
+    exit(-1)
+key = str(input('[+] AES Pre-Shared-Key For Connection :'))
 hashed = hashlib.sha256(key.encode()).digest()
 aes = pyaes.AES(hashed)
 
@@ -49,6 +54,22 @@ def process_text(data): #take data in as a string return 16 bytes block of bytes
         streams.append(stream_bytes)
     return streams
 
+def verify_and_display(recv_dict):
+    timestamp = recv_dict['timestamp']
+    recv_hash = recv_dict['hash']
+    message   = recv_dict['message']
+    mess_hash = hashlib.sha256(str(message).encode('utf-8')).hexdigest()
+    SET_LEN = 80
+    if (mess_hash == recv_hash):
+        tag = str('☑')
+    else:
+        tag = str('☒')
+    spaces = SET_LEN - len(str(message)) - len('Received : ') - 1
+    if spaces > 0 :
+        space = ' '*spaces
+        sentence = 'Received : ' + str(message) + space + tag + '  ' + timestamp
+        print(sentence)
+
 class myThread(threading.Thread):
     def __init__(self,id):
         threading.Thread.__init__(self)
@@ -60,30 +81,49 @@ class myThread(threading.Thread):
     def run(self):
         print("[+] Listening On Thread "+str(self.threadID))
         while 1:
-            data = s.recv(1024)
-            if(data!=""):
-                processed_data = process_bytes(data)
-                print("Recieved : ",end="")
-                for dat in processed_data:
-                    decrypted = aes.decrypt(dat)
-                    mess=''
-                    for ch in decrypted:
-                        if(chr(ch)!='~'):
-                            mess+=str(chr(ch))
-                    print (str(mess),end= "")
-                print("")
+            try:
+
+                data = s.recv(1024)
+                if(data!=""):
+                    mess = ''
+                    processed_data = process_bytes(data)
+                    for dat in processed_data:
+                        decrypted = aes.decrypt(dat)
+                        for ch in decrypted:
+                            if(chr(ch)!='~'):
+                                mess+=str(chr(ch))
+                    try:
+                        data_recv = json.loads(mess)
+                        #message = str(data_recv['message'])
+                        verify_and_display(data_recv)
+                    except:
+                        print('Unrecognised Data or Broken PIPE ')
+            except ConnectionResetError:
+                print('Broken PIPE!')
 
 Listening_Thread = myThread(1)
 Listening_Thread.daemon = True
 Listening_Thread.start()
 
 while 1:
-    sending_data = str(input(""))
+    try:
+        sending_data = str(input(""))
+    except KeyboardInterrupt:
+        s.close()
+        exit(-1)
     if(sending_data=="quit()"):
         Listening_Thread.stop()
         s.close()
         exit()
-    sending_bytes = process_text(sending_data)
+    timestamp = str(datetime.now())[11:19]
+    mess_hash = hashlib.sha256(str(sending_data).encode('utf-8')).hexdigest()
+    send_data = {
+        "timestamp" : timestamp,
+        "message"   : sending_data,
+        "hash"      : mess_hash
+    }
+    send_json_string = json.dumps(send_data)
+    sending_bytes = process_text(send_json_string)
     enc_bytes = []
     for byte in sending_bytes:
         ciphertext = aes.encrypt(byte)
